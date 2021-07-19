c1d036ecce0b5d221b49927a3ca8f3fa6b30176b
ignaesLionel <ignaes_lionel@hotmail.com>
2021-07-19 16:00:50 +0200

    SignIn & Register /w Notify

--- pages/signin.js
+++ pages/signin.js
@@ -1,9 +1,61 @@
 import React from "react";
 import Head from "next/head";
-import { Form, Button } from "react-bootstrap";
 import Link from "next/link";
+import { useState, useRef, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { Form, Button } from "react-bootstrap";
+import { DataContext } from "../store/GlobalState";
+import { postData } from "../utils/fetchData";
+import Cookie from "js-cookie";
 
 const Signin = () => {
+  // Data from input
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+  const dataToSend = { email, password };
+
+  //Routing
+  const router = useRouter();
+
+  useEffect(() => {
+    if (Object.keys(auth).length !== 0) router.push("/");
+  }, [auth]);
+
+  //context & actions reducers
+  const { state, dispatch } = useContext(DataContext);
+  const { auth } = state;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
+    const res = await postData("auth/login", dataToSend);
+
+    //error to notify -> Toast
+    if (res.err)
+      return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+    dispatch({ type: "NOTIFY", payload: { success: res.msg } });
+
+    //Context Auth
+    dispatch({
+      type: "AUTH",
+      payload: {
+        token: res.access_token,
+        user: res.user,
+      },
+    });
+
+    //Cookie Auth
+    Cookie.set("refreshtoken", res.refresh_token, {
+      path: "api/auth/accessToken",
+      expires: 7,
+    });
+
+    //Local Auth
+    localStorage.setItem("firstLogin", true);
+  };
+
   return (
     <div>
       {" "}
@@ -12,20 +64,29 @@ const Signin = () => {
       </Head>
       <h1>SignIn</h1>
       <h1>S’identifier</h1>
-      <Form className="mx-auto my-4" style={{ maxWidth: "500px" }}>
+      <Form
+        className="mx-auto my-4"
+        style={{ maxWidth: "500px" }}
+        onSubmit={handleSubmit}
+      >
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" placeholder="votre email" />
-          <Form.Text className="text-muted">
-            We ll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control
+            type="email"
+            ref={emailInput}
+            value={email}
+            onChange={(e) => setEmail(emailInput.current.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Mot de passe</Form.Label>
-          <Form.Control type="password" placeholder="votre mot de passe" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Control
+            type="password"
+            value={password}
+            ref={passwordInput}
+            onChange={(e) => setPassword(passwordInput.current.value)}
+            placeholder="votre mot de passe"
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Connexion
